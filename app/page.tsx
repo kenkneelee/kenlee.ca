435ec0a2e8235253c09bda09366e4fe1df041f56
Kenny Lee <kenny.lee927@gmail.com>
2024-04-05 20:22:23 -0400

    create next app

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <a href="https://github.com/kenkneelee">Kenny Lee</a>
+      <p>Portfolio under construction!</p>
+    </main>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/kenlee.ca",
+  output: "export",
+  reactStrictMode: true,
+};
+
+export default nextConfig;
