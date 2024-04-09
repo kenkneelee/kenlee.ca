642ce9641f5eea6882ed278ff184aae12c9a69e0
Kenny Lee <kenny.lee927@gmail.com>
2024-04-08 20:18:48 -0400

    linting

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,22 +9,22 @@ export default function Home() {
     <main>
       <Header></Header>
       <Typography variant="h2">
-        Hi! I'm <Link>Kenny.</Link>
+        Hi! I&apos;m <Link>Kenny.</Link>
       </Typography>
       <Typography variant="h6">
-        Hi there! I'm Kenny, a passionate and creative full-stack web
+        Hi there! I&apos;m Kenny, a passionate and creative full-stack web
         developer based in Toronto. I specialize in crafting modern,
         responsive, and user-friendly websites and applications that leave a
         lasting impression. With a strong foundation in front-end technologies
         like HTML, CSS, and JavaScript, coupled with expertise in backend
         frameworks such as Node.js and databases like PostGreSQL, I bring ideas to
-        life from concept to deployment. Whether it's building robust APIs,
+        life from concept to deployment. Whether it&apos;s building robust APIs,
         integrating third-party services, or optimizing performance, I thrive on
         solving complex challenges and delivering scalable solutions. My goal is
         to create seamless digital experiences that not only meet but exceed
-        expectations. I'm constantly learning and adapting to new technologies
+        expectations. I&apos;m constantly learning and adapting to new technologies
         to stay ahead in this ever-evolving industry. Take a look at my
-        portfolio to explore some of my recent projects. Let's collaborate and
+        portfolio to explore some of my recent projects. Let&apos;s collaborate and
         turn your ideas into reality!
       </Typography>
     </main>
