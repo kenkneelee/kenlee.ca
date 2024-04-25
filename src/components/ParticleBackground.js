41f73e744ef83b975a3c8efec7c423a117f41d30
Kenny Lee <kenny.lee927@gmail.com>
2024-04-25 02:34:30 -0400

    use tsparticles library to create animated interactable background

---
+++ src/components/ParticleBackground.js
@@ -0,0 +1,144 @@
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import styles from "./particleBackground.module.css";
+
+const ParticleBackground = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    const initializeParticles = async () => {
+      await initParticlesEngine(async (engine) => {
+        await loadSlim(engine);
+      });
+
+      setInit(true);
+    };
+
+    initializeParticles();
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  return (
+    <div className={styles.particleContainer}>
+      {init && (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={{
+            fullScreen: {
+              enable: true,
+              zIndex: 0,
+            },
+            // background: {
+            //   color: {
+            //     value: "#151722",
+            //   },
+            // },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "grab",
+                },
+                resize: true,
+              },
+              modes: {
+                grab: {
+                  distance: 200,
+                  line_linked: { opacity: 1 },
+                },
+                push: {
+                  quantity: 30,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              number: {
+                value: 200,
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+              },
+              color: {
+                value: "#ffffff",
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 0,
+                  color: "#000000",
+                },
+                polygon: {
+                  nb_sides: 5,
+                },
+                image: {
+                  src: "img/github.svg",
+                  width: 100,
+                  height: 100,
+                },
+              },
+              opacity: {
+                value: 1,
+                random: true,
+                anim: {
+                  enable: true,
+                  speed: 1,
+                  opacity_min: 0,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 1,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 4,
+                  size_min: 0.3,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: false,
+                distance: 150,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1,
+              },
+              move: {
+                enable: true,
+                speed: 1,
+                direction: "none",
+                random: true,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 600,
+                },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ParticleBackground;
