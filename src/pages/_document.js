12bcf25935eb3f6e23caba88785ebf51a1b06f45
Kenny Lee <kenny.lee927@gmail.com>
2024-04-25 02:53:32 -0400

    change document title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Kenny Lee - Full Stack Web Developer</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
